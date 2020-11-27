a9b3f007a9df15ee17290d600fe2266a387e7940
Carlos Pedro <carlospedr@gmail.com>
2020-11-27 16:51:36 +0100

    inital commit

---
+++ src/components/ManageGoals.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="#app">
+      <error-alert v-if="isError">
+        <p>Wrong</p>
+        <button @click="confirmError">Ok</button>
+      </error-alert>
+    </teleport>
+  </div>
+</template>
+<script>
+import ErrorAlert from "./ErrorAlert";
+export default {
+  data() {
+    return {
+      isError: false,
+    };
+  },
+  components: {
+    ErrorAlert,
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (!enteredValue) this.isError = true;
+      else this.isError = false;
+    },
+    confirmError() {
+      this.isError = false;
+    },
+  },
+};
+</script>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <base-card>
+      <template v-slot:header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import BaseBadge from "./BaseBadge";
+import BaseCard from "./BaseCard";
+export default {
+  props: ["fullName", "infoText", "role"],
+  components: { BaseBadge, BaseCard },
+};
+</script>
